b98e06d82ba988714b61899cde00d4bb867ce1d2
Manoel Max <118477075+manoelmax@users.noreply.github.com>
2022-12-06 19:56:22 -0300

    Initial commit

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 78.2
+
+console.log(`Sua velocidade é ${vel}km/h`)
+
+if(vel > 60){
+    console.log('Ultrapassou o limite de velocidade.Você foi multado!')
+}
+
+console.log('Dirija com segurança!')
