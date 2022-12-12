fe7326fff1257a3eb042e3f929f386882aa30701
Manoel Max <118477075+manoelmax@users.noreply.github.com>
2022-12-12 01:25:50 -0300

    finalizei exercício 18

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -22,8 +22,42 @@ function inLista(n, l){
 
 function adicionar(){
     if(isNumero(num.value) && !inLista(num.value, valores)){
-        alert('tudo ok')
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado!`
+        lista.appendChild(item)
+        res.innerHTML = ' '
     }else{
         alert('Valor inválido ou já esta na lista!')
+    }
+    num.value = ''
+    num.focus()/*coloca o cursor na caixa de texto*/
+}
+
+function finalizar(){
+    if(valores.length == 0){
+        alert('Adicione valores antes de finalizar!')
+    }else{
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores){
+            soma += valores[pos]
+            if(valores[pos] > maior){
+                maior = valores[pos]
+            }
+            if(valores[pos] < menor){
+                menor = valores[pos]
+            }
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `Ao todo temos ${tot} números cadastrados`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+        res.innerHTML += `<p>A soma de todos os valores é igual a ${soma}</p>`
+        res.innerHTML += `<p>A media dos valores digitados é igual a ${media}</p>`
     }
 }
