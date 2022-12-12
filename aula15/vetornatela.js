066ec44815f40cb4ab7049b866f50f3a84ca1122
Manoel Max <118477075+manoelmax@users.noreply.github.com>
2022-12-11 22:46:11 -0300

    finalizei aula 15

---
+++ aula15/vetornatela.js
@@ -0,0 +1,9 @@
+let valores = [3, 4, 7, 9, 5]
+/*
+for(let pos = 0;pos < valores.length;pos++){
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
+*/
+for(let pos in valores){
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
